d7cc8858d7f722dbaba2cae6e5a6226772296cb2
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-11-14 19:44:24 +0100

    add input event listeners

--- script.js
+++ script.js
@@ -34,3 +34,61 @@ const addressResume = document.getElementById('addressResume');
 addressInput.addEventListener("change", (e) => {
     addressResume.textContent = e.target.value;
 })
+
+const schoolInput = document.getElementById('schoolInput');
+const degreeInput = document.getElementById('degreeInput');
+const startDateEducationInput = document.getElementById('startDateEducationInput');
+const endDateEducationInput = document.getElementById('endDateEducationInput');
+
+
+const schoolResume = document.querySelector('.school');
+
+schoolInput.addEventListener("change", (e) =>{
+    schoolResume.textContent = e.target.value
+})
+
+const degreeResume = document.querySelector('.field');
+
+degreeInput.addEventListener("change", (e) => {
+    degreeResume.textContent = e.target.value;
+})
+
+const startDateResume = document.querySelector('span.start');
+
+startDateEducationInput.addEventListener("change", (e) => {
+    startDateResume.textContent = e.target.value;
+})
+
+const endDateResume = document.querySelector('span.end');
+
+endDateEducationInput.addEventListener("change", (e) => {
+    endDateResume.textContent = ' - ' +  e.target.value;
+})
+
+const companyInput = document.getElementById('companyInput');
+const titleInput = document.getElementById('titleInput');
+const startDateExperienceInput = document.getElementById('startDateExperienceInput');
+const endDateExperienceInput = document.getElementById('endDateExperienceInput');
+
+const companyResume = document.querySelector('.company');
+
+companyInput.addEventListener("change", (e) => {
+    companyResume.textContent = e.target.value;
+})
+
+const titleResume = document.querySelector('.title');
+
+titleInput.addEventListener("change", (e) => {
+    titleResume.textContent = e.target.value;
+})
+
+const startDateExperienceResume = document.getElementById('start-experience');
+const endDateExperienceResume = document.getElementById('end-experience');
+
+startDateExperienceInput.addEventListener("change", (e) => {
+    startDateExperienceResume.textContent = e.target.value
+})
+
+endDateExperienceInput.addEventListener("change", (e) => {
+    endDateExperienceResume.textContent = ' - ' + e.target.value
+})
